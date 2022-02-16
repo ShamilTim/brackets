224e3ac744bcdbf45931fcc2269d6b1912ef4a65
stimirgaleev <you@example.com>
2022-02-16 12:03:19 +0300

    feat: add brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const brackets = bracketsConfig.map(i => i.join(''));
+    let result = str;
+    let flag = true;
+    while (flag) {
+        let condition = result;
+        brackets.forEach(el => result = result.replace(el, ''));
+        flag = result == condition ? false : true; 
+    }
+    return !result;
 }
